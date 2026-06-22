43dde50877b9f6a5b013f4d8f8e8fe58bb32802f
unknown <juan.oviedo.lutkens@gmail.com>
2026-06-22 10:40:17 -0500

    feat: add hotjar tracking code script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,24 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Meta Pixel Code */}
+
+        {/* Hotjar Tracking Code */}
+        {process.env.NEXT_PUBLIC_HOTJAR_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `,
+            }}
+          />
+        )}
       </head>
       <body>{children}</body>
     </html>
